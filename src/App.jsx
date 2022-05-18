a009abc08e7396399d8c07481b03abaa1b2e1eeb
Eric Saul <ericmsaul@gmail.com>
2022-05-18 11:02:29 -0700

    Setup basic routes w React Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
