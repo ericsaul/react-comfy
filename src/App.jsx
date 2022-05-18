38777b1461c4147d7313067313d4024ae7014f13
Eric Saul <ericmsaul@gmail.com>
2022-05-18 12:42:49 -0700

    Added navbar, footer and sidebar inside the <Router /> component on App component, so they appear on all pages.

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,18 @@ import { Navbar, Footer, Sidebar } from "./components/index";
 const App = () => {
   return (
     <Router>
+      <Navbar />
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="cart" element={<Cart />} />
         <Route path="products" element={<Products />} />
-        <Route path="products:id" element={<SingleProduct />} />
+        <Route path="products/:id" element={<SingleProduct />} />
         <Route path="checkout" element={<Checkout />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
