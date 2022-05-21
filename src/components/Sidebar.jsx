05360bb8752183d3821c6472440ffd0026031fc2
Eric Saul <ericmsaul@gmail.com>
2022-05-21 12:53:23 -0700

    Setup basics for sidebar. Includes bringing in the id, text and url for navigation from utils/contsants (using map()).

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,10 +6,32 @@ import { FaTimes } from "react-icons/fa";
 import { links } from "../utils/constants";
 
 const Sidebar = () => {
+  const isOpen = false;
+
   return (
-    <div>
-      <h2>Sidebar</h2>
-    </div>
+    <SidebarContainer>
+      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+        <div className="sidebar-header">
+          <img src={logo} alt="comfy sloth" className="logo" />
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="links">
+          {links.map(({ id, text, url }) => {
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+          <li>
+            <Link to="/checkout">checkout</Link>
+          </li>
+        </ul>
+        <CartButtons />
+      </aside>
+    </SidebarContainer>
   );
 };
 
