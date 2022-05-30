1a0f65a4828e786667812f7b2da8e92d7a46cdc5
Eric Saul <ericmsaul@gmail.com>
2022-05-30 11:47:33 -0700

    Added AddToCart's increase, decreaes amount functionality. Increase and decrease fns were created in AddToCart component (and the amount) and passed down to AmountButtons component. The increase/decrease fns check to see if amount is greater than stock or less than one.

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -8,30 +8,59 @@ import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
 
   return (
     <Wrapper>
       <div className="colors">
-        <div className="btn-container">
-          <span className="color">colors: </span>
-          <div>
-            {colors.map((color, index) => {
-              return (
-                <button
-                  key={index}
-                  type="button"
-                  className={`${
-                    color === mainColor ? "color-btn active" : "color-btn"
-                  }`}
-                  style={{ background: color }}
-                  onClick={() => setMainColor(color)}
-                >
-                  {color === mainColor && <FaCheck />}
-                </button>
-              );
-            })}
-          </div>
+        <span className="color">colors: </span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                type="button"
+                className={`${
+                  color === mainColor ? "color-btn active" : "color-btn"
+                }`}
+                style={{ background: color }}
+                onClick={() => setMainColor(color)}
+              >
+                {color === mainColor && <FaCheck />}
+              </button>
+            );
+          })}
         </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   );
