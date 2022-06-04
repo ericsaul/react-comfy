234854e2c67af00e2d3b87dfb4f9863a0d3f26b0
Eric Saul <ericmsaul@gmail.com>
2022-06-04 07:51:29 -0700

    Finished basic setup of cart context (adding properties to initial state, CartProvider on main.js, useReducer setup in cart_context).

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -1,7 +1,12 @@
 import { useContext, useReducer, createContext } from "react";
 import reducer from "../reducers/cart_reducer";
 
-const initialState = {};
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534,
+};
 
 const CartContext = createContext();
 
@@ -14,7 +19,17 @@ import {
 } from "../actions";
 
 export const CartProvider = ({ children }) => {
-  return <CartContext.Provider value="hello">{children}</CartContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 };
 
 export const useCartContext = () => useContext(CartContext);
