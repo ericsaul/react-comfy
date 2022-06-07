69c81f91f09f9a3e847656cf4bcd7a52b6d3dc70
Eric Saul <ericmsaul@gmail.com>
2022-06-07 13:10:36 -0700

    Setup cart subtotal, shipping fee, and order total in CartTotals component. Also created button to continue to 'proceed to checkout.

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -5,10 +5,27 @@ import { formatPrice } from "../utils/helpers";
 import { Link } from "react-router-dom";
 
 const CartTotals = () => {
+  const { total_amount, shipping_fee } = useCartContext();
   return (
-    <div>
-      <h2>Cart Totals</h2>
-    </div>
+    <Wrapper>
+      <div>
+        <article>
+          <h5>
+            subtoal: <span>{formatPrice(total_amount)}</span>
+          </h5>
+          <p>
+            shipping fee: <span>{formatPrice(shipping_fee)}</span>
+          </p>
+          <hr />
+          <h4>
+            order total: <span>{formatPrice(total_amount + shipping_fee)}</span>
+          </h4>
+        </article>
+        <Link to="/checkout" className="btn">
+          proceed to checkout
+        </Link>
+      </div>
+    </Wrapper>
   );
 };
 
