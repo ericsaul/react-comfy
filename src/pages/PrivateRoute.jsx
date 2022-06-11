2836e709f60e8bcfdcf5e428376e2f2daadb71ae
Eric Saul <ericmsaul@gmail.com>
2022-06-11 15:13:42 -0700

    Created private route to the checkout page. It is available if someone is logged out, but the enter the path. Using react router v6.

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,9 +1,13 @@
-const PrivateRoute = () => {
-  return (
-    <div>
-      <h2>PrivateRoute</h2>
-    </div>
-  );
+import { Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useUserContext } from "../context/user_context";
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth0();
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+  return children;
 };
 
 export default PrivateRoute;
