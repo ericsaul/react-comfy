44705be31e1d29419a2058d2a8cbffc7d26d884a
Eric Saul <ericmsaul@gmail.com>
2022-06-11 12:40:18 -0700

    Hide checkout in navbar, sidebar and cart page when not logged in. The cart is either login or go to cart.

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -3,9 +3,11 @@ import { useCartContext } from "../context/cart_context";
 // import { useUserContext } from "../components/user_context";
 import { formatPrice } from "../utils/helpers";
 import { Link } from "react-router-dom";
+import { useUserContext } from "../context/user_context";
 
 const CartTotals = () => {
   const { total_amount, shipping_fee } = useCartContext();
+  const { myUser, loginWithRedirect } = useUserContext();
   return (
     <Wrapper>
       <div>
@@ -21,9 +23,15 @@ const CartTotals = () => {
             order total: <span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
-        <Link to="/checkout" className="btn">
-          proceed to checkout
-        </Link>
+        {myUser ? (
+          <Link to="/checkout" className="btn">
+            proceed to checkout
+          </Link>
+        ) : (
+          <button className="btn" type="button" onClick={loginWithRedirect}>
+            Login
+          </button>
+        )}
       </div>
     </Wrapper>
   );
